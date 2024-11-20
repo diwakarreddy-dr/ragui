d0fce98cb567f343934814e870f5b485d13e923a
nitishkumar149345 <nitishmech149@gmail.com>
2024-11-20 15:22:52 +0530

    add sample query

--- src/components/SampleQueries.js
+++ src/components/SampleQueries.js
@@ -2,21 +2,28 @@ export const sampleQueries = [
   {
     id: 1,
     title: "Most Active Physician",
-    query: "Find the physician who has assisted the most procedures. Display their name, position, and the count of procedures they assisted."
+    query: "Find the physician who has assisted the most procedures. Display their name, position, and the count of procedures they assisted.",
   },
   {
     id: 2,
     title: "Multi-Department Physicians",
-    query: "Find all physicians affiliated with multiple departments and the treatments they are trained for in each department."
+    query: "Find all physicians affiliated with multiple departments and the treatments they are trained for in each department.",
+
   },
   {
     id: 3,
     title: "Nurse Procedure Analysis",
-    query: "List all registered nurses who assisted at least one procedure. Include their name, the count of procedures they assisted, and the names of the physicians they worked with."
+    query: "List all registered nurses who assisted at least one procedure. Include their name, the count of procedures they assisted, and the names of the physicians they worked with.",
+
   },
   {
     id: 4,
     title: "Patient Procedure Costs",
     query: "Calculate the total cost of procedures undergone by each patient and list their names alongside the total cost. Sort the result by the total cost in descending order.",
+  },
+  {
+    id: 5,
+    title: "Most Active Nurse",
+    query: "For each nurse, find the total number of patients they assisted during procedures. Include their name and total count."
   }
 ]; 
